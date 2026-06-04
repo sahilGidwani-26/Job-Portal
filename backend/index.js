1e5a24aa3d43e418f417cc4678a987fb304f381b
Sahil Gidwani <gidwanisahil80@gmail.com>
2026-06-04 20:23:30 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: [
+    "http://localhost:5173",
+    "https://job-portal-ruddy-ten.vercel.app"
+  ],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 app.use(cors(corsOptions));
 
